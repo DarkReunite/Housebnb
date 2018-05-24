ef91ae1e6e0679fb04ee18106cda64c545bde602
DarkReunite <924358476@qq.com>
2018-05-24 13:11:04 +0800

    更新注册框的响应式代码和css代码的规范

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,14 @@
-function show() {
-  alert("hello world")
+function show(){
+  document.getElementById('RegLogmodel').style.display='block';
+}
+function hide(){
+  document.getElementById('RegLogmodel').style.display='none';
+}
+
+var model = document.getElementById('RegLogmodel');
+
+window.onclick = function(event){
+  if(event.target == model){
+    model.style.display = 'none';
+  }
 }
